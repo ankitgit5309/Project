854b71b67a2b97bd99abcc40140811af6a2d97d3
ankitgit5309 <109146182+ankitgit5309@users.noreply.github.com>
2024-10-24 16:45:56 +0530

    commit

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     // int a, b, c;
     // short sa=9;
 
-    /*This code will give an error because variables can't be defined 
+    /*This code will give an error because variables can't be defined
      using numbers inthe first palce it should be done after alphabets.*/
 
     // short 1_sa = 9;
@@ -14,15 +15,15 @@ int main(){
     // short s2323a = 9;
     // cout<< s2323a;
 
-// camelCase Notations
+    // camelCase Notations
     // int marksInMaths=83;
     // cout<<marksInMaths <<endl;
     // cout<<"the marks of student in maths is "<<marksInMaths;
-     
+
     // short a;
     // int const b=98;
     // long c;
-    // long long d; 
+    // long long d;
     // float const score =45.32;
     // // float score = 45.32;
     // /*jitna prescise chahie us hisab se variable define karna padega*/
@@ -40,25 +41,42 @@ int main(){
     // cout <<"a-b is " <<a-b <<endl;
     // cout <<"a*b is " <<a*b <<endl;
     // cout <<"a/b is " <<(float)a/b <<endl;
-//     // cout<<"hello world Ankit"<<endl;
-//     // cout<<"Next  line";
-int age;
+    //     // cout<<"hello world Ankit"<<endl;
+    //     // cout<<"Next  line";
+    int age;
 
-cout<<"Enter your age" <<endl;
-cin>>age;
+    cout << "Enter your age" << endl;
+    cin >> age;
+    // Learning of if-else loop
+    //  if(age>150 || age<1)
+    //  {
+    //      cout<<"invalid age";
+    //  }
 
-if(age>150)
-{
-    cout<<"invalid age";
-}
+    // else if (age>= 18)
+    // {
+    //     cout<<"you can vote";
+    // }
 
-else if (age>= 18)
-{
-    cout<<"you can vote";
-}
+    // else
+    // {
+    //     cout<<"you cannot vote";
+    // }
 
-else 
-{
-    cout<<"you cannot vote";
-}
+    // Switch statements
+
+    switch (age)
+    {
+    case 12:
+        cout << "You are 12 years of age";
+        break;
+
+    case 18:
+        cout << "You are 18 years old";
+        break;
+
+    default:
+        cout << "you are neither 12 nor 18 years old";
+        break;
+    }
 }
