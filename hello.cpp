008d3c083d64cd118f8402c64b3ddfcbde503892
ankitgit5309 <109146182+ankitgit5309@users.noreply.github.com>
2024-10-24 23:17:03 +0530

    commit

--- hello.cpp
+++ hello.cpp
@@ -68,15 +68,15 @@ int main()
     switch (age)
     {
     case 12:
-        cout << "You are 12 years of age";
+        cout << "You are 12 years of age" <<endl;
         break;
 
     case 18:
-        cout << "You are 18 years old";
+        cout << "You are 18 years old" <<endl;
         break;
 
     default:
-        cout << "you are neither 12 nor 18 years old";
+        cout << "you are neither 12 nor 18 years old" <<endl;
         break;
     }
 }
