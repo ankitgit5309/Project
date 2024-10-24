22f614423382fddf75d51579f224409fa792a061
ankitgit5309 <109146182+ankitgit5309@users.noreply.github.com>
2024-10-24 15:14:14 +0530

    commit

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int a, b, c;
+    // int a, b, c;
     // short sa=9;
 
     /*This code will give an error because variables can't be defined 
@@ -11,11 +11,22 @@ int main(){
     // short 1_sa = 9;
     // cout << 1_sa;
     /*This is also a valid format*/
-    short s2323a = 9;
-    cout<< s2323a;
+    // short s2323a = 9;
+    // cout<< s2323a;
 
+// camelCase Notations
+    // int marksInMaths=83;
+    // cout<<marksInMaths <<endl;
+    // cout<<"the marks of student in maths is "<<marksInMaths;
      
-
+    short a;
+    int b;
+    long c;
+    long long d; 
+    float score = 45.32;
+    double score2 = 45.332;
+    long double score3 = 45.332;
+    cout<<"The score is" <<score;
 //     // cout<<"hello world Ankit"<<endl;
 //     // cout<<"Next  line";
 
