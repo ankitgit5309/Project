b9863532689f56c13cab73a2352069668fe7f249
ankitgit5309 <109146182+ankitgit5309@users.noreply.github.com>
2024-10-24 15:01:23 +0530

    commit

--- hello.cpp
+++ hello.cpp
@@ -5,16 +5,19 @@ int main(){
     int a, b, c;
     // short sa=9;
 
-    //This code will give an error because variables can't be defined 
-    // using numbers inthe first palce it should be done after alphabets.
+    /*This code will give an error because variables can't be defined 
+     using numbers inthe first palce it should be done after alphabets.*/
 
     // short 1_sa = 9;
     // cout << 1_sa;
+    /*This is also a valid format*/
+    short s2323a = 9;
+    cout<< s2323a;
 
-    
      
 
 //     // cout<<"hello world Ankit"<<endl;
 //     // cout<<"Next  line";
+
     return 0;
-}
+    }
