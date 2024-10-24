b995e341c4bb275dd2f0dfc1be2f573efbec74a1
ankitgit5309 <109146182+ankitgit5309@users.noreply.github.com>
2024-10-24 14:57:45 +0530

    commit

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 int main(){
     int a, b, c;
-    short sa=9;
-    // cout<<"hello world Ankit"<<endl;
-    // cout<<"Next  line";
+    // short sa=9;
+
+    //This code will give an error because variables can't be defined 
+    // using numbers inthe first palce it should be done after alphabets.
+
+    // short 1_sa = 9;
+    // cout << 1_sa;
+
+    
+     
+
+//     // cout<<"hello world Ankit"<<endl;
+//     // cout<<"Next  line";
     return 0;
 }
