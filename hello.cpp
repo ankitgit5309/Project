f55433119eb836b03c74079441e6bc3f46537339
ankitgit5309 <109146182+ankitgit5309@users.noreply.github.com>
2024-10-29 00:44:03 +0530

    commit

--- hello.cpp
+++ hello.cpp
@@ -131,11 +131,13 @@ int main()
         {1,2,3},
         {4,5,6}
     };
-
     for (int i = 0; i < 2; i++)
     {
-        for (int i = 0; j <3; j++)
+        for (int j = 0; j <3; j++)
         {
-            cout<<"The value at", "<<i<<", "<<j<<" is" <<arr2d[i][j];
+            cout<<"The value at "<<i<<", "<<j<<" is" <<arr2d[i][j]<<endl;
         }
     }
+    
+    return 0;   
+}
